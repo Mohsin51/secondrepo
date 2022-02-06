dd9f6280847a2556742e21660f040f58b8a95483
bilal <bilalmohsin49@gmail.com>
2022-02-06 22:17:59 +0500

    my second code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Covid from './components/covid';
+
+function App() {
+  return (
+    <>
+    <Covid />
+    </>
+  );
+}
+
+export default App;
